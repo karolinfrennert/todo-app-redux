f79c4bd5e7cb08df9c3da148c579cd08225098a9
Karolin Frennert <karolin@alextherapeutics.com>
2021-10-20 15:29:49 +0200

    Added asynThunk fetching todos

--- src/store.js
+++ src/store.js
@@ -1,12 +1,12 @@
-import { createStore, applyMiddleware } from 'redux'
-import thunkMiddleware from 'redux-thunk'
-import { composeWithDevTools } from 'redux-devtools-extension'
-import rootReducer from './reducer'
+import { configureStore } from '@reduxjs/toolkit'
+import todosReducer from './features/todos/todosSlice'
+import filtersReducer from './features/filters/filtersSlice'
 
-const composedEnhancer = composeWithDevTools(
-  applyMiddleware(thunkMiddleware)
-  // other store enhancers if any
-)
+const store = configureStore({
+  reducer: {
+    todos: todosReducer,
+    filters: filtersReducer,
+  },
+})
 
-const store = createStore(rootReducer, composedEnhancer)
 export default store
